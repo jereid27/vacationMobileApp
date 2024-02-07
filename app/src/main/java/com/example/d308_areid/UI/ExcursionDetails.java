0353067b29ac15f553998f5a8bcebffba66cddec
Jude <jelijreid27@gmail.com>
2024-02-06 22:33:14 -0600

    B4. Included the required details for each excursion.

--- app/src/main/java/com/example/d308_areid/UI/ExcursionDetails.java
+++ app/src/main/java/com/example/d308_areid/UI/ExcursionDetails.java
@@ -1,4 +1,59 @@
 package com.example.d308_areid.UI;
 
-public class ExcursionDetails {
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.d308_areid.R;
+import com.example.d308_areid.database.Repository;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class ExcursionDetails extends AppCompatActivity {
+
+    String name;
+    int excursionID;
+    int vacationID;
+    String excursionDate;
+    String endDateThing;
+    String startDateThing;
+    String note;
+    EditText editName;
+    EditText editNote;
+    TextView editDate;
+    Repository repository;
+    Date checkExcursionDateObj;
+    DatePickerDialog.OnDateSetListener startDate;
+    final Calendar myCalendarStart = Calendar.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_excursion_details);
+        repository = new Repository(getApplication());
+
+        name = getIntent().getStringExtra("name");
+        editName = findViewById(R.id.excursionTitleText);
+        editName.setText(name);
+
+        excursionID = getIntent().getIntExtra("id", -1);
+        vacationID = getIntent().getIntExtra("vacationID", -1);
+
+        excursionDate = getIntent().getStringExtra("excursionDate");
+
+
+        note = getIntent().getStringExtra("note");
+        editNote = findViewById(R.id.excursionnote);
+        editNote.setText(note);
+
+        editDate = findViewById(R.id.excursiondate);
+        editDate.setText(excursionDate);
+
+        startDateThing = getIntent().getStringExtra("startDate");
+        endDateThing = getIntent().getStringExtra("endDate");
+    }
 }
